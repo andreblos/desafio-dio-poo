74f90aaf2d671efe7c0ef7f2846e526ba85abea3
André Blos Aliatti <andreblos82@gmail.com>
2025-08-15 11:13:37 -0300

    Criação classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHorario(4);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHorario(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("descrição mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
     }
 }
